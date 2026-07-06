5b96366dcd1b6e5972d00c045c7d0dee198d3f75
LOrtizarq <luis.leon.ortiz.12@gmail.com>
2026-07-07 00:25:30 +0200

    arreglo cifras

--- lib/utils/formato.ts
+++ lib/utils/formato.ts
@@ -1,8 +1,9 @@
 const _fmt = new Intl.NumberFormat('es-ES', {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
+  useGrouping: 'always',
 });
 
 export function formatearMoneda(valor: number): string {
-  return _fmt.format(valor) + ' €';
+  return _fmt.format(valor) + ' €';
 }
